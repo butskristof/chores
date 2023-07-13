20cc6924ff84c1136c1a638570a458597bdd156a
Kristof Buts <git@kristofbuts.be>
2023-07-13 23:27:52 +0200

    list item

---
+++ client-poc/src/pages/ChoreListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="chore-list-item">
+    <div class="header">
+      <div class="title is-large">Chore</div>
+    </div>
+    <div class="description">Some description</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+@import '@/styles/_variables.scss';
+
+.chore-list-item {
+  padding: $box-padding;
+  border: 1px solid black;
+  border-radius: var(--hiq-border-radius);
+}
+</style>
--- client-poc/src/views/HomeView.vue
+++ client-poc/src/views/HomeView.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home">
-    <div>hello</div>
+    <div class="chores">
+      <ChoreListItem />
+      <ChoreListItem />
+      <ChoreListItem />
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import ChoreListItem from '@/pages/ChoreListItem.vue';
+</script>
+
+<style scoped lang="scss">
+.chores {
+  > * {
+    margin-bottom: 1rem;
+  }
+}
+</style>
