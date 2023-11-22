9f6abdce7453abe5ec13e1b99e2f6c288fa2227d
Kristof Buts <kristof.buts@marqiz.com>
2023-11-22 22:22:02 +0100

    linting & formatting

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), eslint()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
