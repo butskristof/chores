4e05194defbb8eea29c19859a9e4548ea763aa46
Kristof Buts <git@kristofbuts.be>
2023-11-24 11:11:22 +0100

    add env

---
+++ client/src/utilities/env.js
@@ -0,0 +1,9 @@
+// these getters try to retrieve a value from either the config.js file (generated on
+// container startup to support runtime environment variables), or the 'normal' dotenv
+// environment during development
+const getEnv = (name) => window?.configs?.[name] ?? import.meta.env[name];
+const getEnvBool = (name) => getEnv(name) === 'true';
+
+export const OIDC_AUTHORITY = getEnv('VITE_OIDC_AUTHORITY');
+export const OIDC_CLIENT_ID = getEnv('VITE_OIDC_CLIENT_ID');
+export const OIDC_LOGGING = getEnvBool('VITE_OIDC_LOGGING');
