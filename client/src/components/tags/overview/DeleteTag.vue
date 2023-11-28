69ea2baf8b94df94001e9f21ed9def7ec07bf75f
Kristof Buts <kristof.buts@marqiz.com>
2023-11-28 14:09:04 +0100

    crud elements

---
+++ client/src/components/tags/overview/DeleteTag.vue
@@ -0,0 +1,24 @@
+<template>
+  <Dialog
+    :open="open"
+    @close="emit('close')"
+  >
+    <DialogPanel>
+      <DialogTitle>Delete tag</DialogTitle>
+      <div>hey</div>
+    </DialogPanel>
+  </Dialog>
+</template>
+
+<script setup>
+import { Dialog, DialogPanel, DialogTitle } from '@headlessui/vue';
+defineProps({
+  open: {
+    type: Boolean,
+    default: false,
+  },
+});
+const emit = defineEmits(['close']);
+</script>
+
+<style scoped lang="scss"></style>
---
+++ client/src/components/tags/overview/EditTag.vue
@@ -0,0 +1,36 @@
+<template>
+  <Dialog
+    :open="open"
+    @close="emit('close')"
+  >
+    <DialogPanel>
+      <DialogTitle>{{ title }}</DialogTitle>
+      <div>hey</div>
+      <div v-if="props.tag != null">
+        <pre>{{ JSON.stringify(props.tag, null, 2) }}</pre>
+      </div>
+    </DialogPanel>
+  </Dialog>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { Dialog, DialogPanel, DialogTitle } from '@headlessui/vue';
+
+const props = defineProps({
+  open: {
+    type: Boolean,
+    default: false,
+  },
+  tag: {
+    type: Object,
+    default: null,
+  },
+});
+const emit = defineEmits(['close']);
+
+const isEdit = computed(() => props.tag != null);
+const title = computed(() => (isEdit.value === true ? 'Edit tag' : 'Add a new tag'));
+</script>
+
+<style scoped lang="scss"></style>
