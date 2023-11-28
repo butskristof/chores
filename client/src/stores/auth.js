90022cb93d63d20086082ae322c0c607967f1600
Kristof Buts <kristof.buts@marqiz.com>
2023-11-28 12:34:21 +0100

    refactor authentication

--- client/src/stores/auth.js
+++ client/src/stores/auth.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { computed, ref, watch } from 'vue';
+import { computed, ref } from 'vue';
 
 export const useAuthStore = defineStore('auth', () => {
   const user = ref(null);
@@ -7,18 +7,16 @@ export const useAuthStore = defineStore('auth', () => {
   //#region getters
 
   const isAuthenticated = computed(() => user.value != null && user.value.expired !== true);
-  const accessToken = computed(() => (isAuthenticated.value ? user.value.access_token : null));
-  const profile = computed(() => (isAuthenticated.value ? user.value.profile : null));
+  const accessToken = computed(() =>
+    isAuthenticated.value === true ? user.value.access_token : null,
+  );
+  const profile = computed(() => (isAuthenticated.value === true ? user.value.profile : null));
 
   //#endregion
 
   //#region actions
 
   const setUser = (value) => (user.value = value);
-
-  watch(user, () => {
-    console.log('auth store: user updated', user.value);
-  });
 
   //#endregion
 
