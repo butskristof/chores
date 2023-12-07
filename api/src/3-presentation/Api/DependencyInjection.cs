bb5d32c3003146f8755ee69bf63bfb3b7eb31843
Kristof Buts <git@kristofbuts.be>
2023-12-07 17:39:57 +0100

    documentation

--- api/src/3-presentation/Api/DependencyInjection.cs
+++ api/src/3-presentation/Api/DependencyInjection.cs
@@ -32,6 +32,10 @@ internal static class DependencyInjection
             });
 
         // add support for ProblemDetails to handle failed requests
+        // it's effectively a default implementation of the IProblemDetailsService and can be
+        // overridden if desired
+        // it adds a default problem details response for all client and server error responses (400-599)
+        // that don't have body content yet
         services.AddProblemDetails();
 
         return services;
--- api/src/3-presentation/Api/Program.cs
+++ api/src/3-presentation/Api/Program.cs
@@ -18,8 +18,10 @@ app
     // them as ProblemDetails with status code 500 Internal Server Error
     .UseExceptionHandler()
     // the status code pages will map additional failed requests (outside of
-    // those throwing exceptions) to ProblemDetails responses
-    // this includes 404, method not allowed, ...
+    // those throwing exceptions) to responses with ProblemDetails body content
+    // this includes 404, method not allowed, ... (all status codes between 400 and 599)
+    // keep in mind that this middleware will only activate if the body is empty when
+    // it reaches it
     .UseStatusCodePages();
 
 // add endpoint to retrieve OpenAPI definition
