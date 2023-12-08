0b8911c811b6c5c818ee695dda838d03552833f5
Kristof Buts <git@kristofbuts.be>
2023-12-08 01:18:23 +0100

    set up test project

---
+++ api/tests/1-core/Application.IntegrationTests/Modules/Tags/CreateTagTests.cs
@@ -0,0 +1,10 @@
+namespace Chores.Application.IntegrationTests.Modules.Tags;
+
+public sealed class CreateTagTests
+{
+    [Fact]
+    public void Test()
+    {
+        Assert.True(false);
+    }
+}
