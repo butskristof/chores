384a1d2ce7eb9d2df71be5e5906cf7c1b7c8bb58
Kristof Buts <git@kristofbuts.be>
2023-12-12 18:45:17 +0100

    inner fixture mess

--- api/tests/1-core/Application.IntegrationTests/Modules/Tags/CreateTagTests.cs
+++ api/tests/1-core/Application.IntegrationTests/Modules/Tags/CreateTagTests.cs
@@ -4,39 +4,77 @@ using ErrorOr;
 
 namespace Chores.Application.IntegrationTests.Modules.Tags;
 
-[Collection(nameof(TestFixture))]
-public sealed class CreateTagTests : TestBase
+// [Collection(nameof(TestFixture))]
+// public sealed class CreateTagTests : TestBase
+// {
+//     public CreateTagTests(TestFixture fixture) : base(fixture)
+//     {
+//     }
+//
+//     [Fact]
+//     public async Task InvalidRequest_ReturnsValidationError()
+//     {
+//         var request = new CreateTag.Request(string.Empty);
+//         var result = await Fixture.SendAsync(request);
+//         Assert.True(result.IsError);
+//         var error = result.ErrorsOrEmptyList.SingleOrDefault();
+//         Assert.Equal(ErrorType.Validation, error.Type);
+//     }
+//     
+//     [Fact]
+//     public async Task CreatesTag()
+//     {
+//         var request = new CreateTag.Request("hey");
+//         var response = await Fixture.SendAsync(request);
+//         Assert.False(response.IsError);
+//         Assert.NotEqual(Guid.Empty, response.Value.Id);
+//         Assert.Equal("hey", response.Value.Name);
+//     }
+//
+//     public sealed class RequestWithValidationErrors
+//     {
+//         [Fact]
+//         public void ReturnsValidationError()
+//         {
+//             
+//         }
+//     }
+// }
+
+public static class CreateTagTests
 {
-    public CreateTagTests(TestFixture fixture) : base(fixture)
+    [Collection(nameof(TestFixture))]
+    public sealed class InvalidRequest : TestBase, IClassFixture<InvalidRequest.InnerFixture>
     {
-    }
-
-    [Fact]
-    public async Task InvalidRequest_ReturnsValidationError()
-    {
-        var request = new CreateTag.Request(string.Empty);
-        var result = await Fixture.SendAsync(request);
-        Assert.True(result.IsError);
-        var error = result.ErrorsOrEmptyList.SingleOrDefault();
-        Assert.Equal(ErrorType.Validation, error.Type);
-    }
-    
-    [Fact]
-    public async Task CreatesTag()
-    {
-        var request = new CreateTag.Request("hey");
-        var response = await Fixture.SendAsync(request);
-        Assert.False(response.IsError);
-        Assert.NotEqual(Guid.Empty, response.Value.Id);
-        Assert.Equal("hey", response.Value.Name);
-    }
-
-    public sealed class RequestWithValidationErrors
-    {
-        [Fact]
-        public void ReturnsValidationError()
+        public InvalidRequest(TestFixture fixture, InnerFixture innerFixture) : base(fixture)
         {
-            
+            _fixture = innerFixture;
+        }
+
+        public class InnerFixture : IAsyncLifetime
+        {
+            public ErrorOr<CreateTag.Response> Result { get; }
+
+            public async Task InitializeAsync()
+            {
+                var request = new CreateTag.Request(string.Empty);
+                var result = await Fixture.SendAsync(request);
+            }
+
+            public Task DisposeAsync()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private readonly InnerFixture _fixture;
+
+        [Fact]
+        public async Task ReturnsValidationResult()
+        {
+            Assert.True(result.IsError);
+            var error = result.Errors.SingleOrDefault();
+            Assert.Equal(ErrorType.Validation, error.Type);
         }
     }
 }
