b63e82b01776af191b2782b23fb0fa4ae3d44728
Kristof Buts <git@kristofbuts.be>
2023-12-12 17:24:06 +0100

    add create tag tests

--- api/tests/1-core/Application.IntegrationTests/Modules/Tags/CreateTagTests.cs
+++ api/tests/1-core/Application.IntegrationTests/Modules/Tags/CreateTagTests.cs
@@ -1,5 +1,6 @@
 using Chores.Application.IntegrationTests.Common;
 using Chores.Application.Modules.Tags;
+using ErrorOr;
 
 namespace Chores.Application.IntegrationTests.Modules.Tags;
 
@@ -9,22 +10,33 @@ public sealed class CreateTagTests : TestBase
     public CreateTagTests(TestFixture fixture) : base(fixture)
     {
     }
-    
+
     [Fact]
-    public async Task Test()
+    public async Task InvalidRequest_ReturnsValidationError()
     {
-        var request = new CreateTag.Request("hey");
-        var response = await Fixture.SendAsync(request);
-        Assert.False(response.IsError);
-        Assert.Equal("hey", response.Value.Name);
+        var request = new CreateTag.Request(string.Empty);
+        var result = await Fixture.SendAsync(request);
+        Assert.True(result.IsError);
+        var error = result.ErrorsOrEmptyList.SingleOrDefault();
+        Assert.Equal(ErrorType.Validation, error.Type);
     }
     
     [Fact]
-    public async Task Test2()
+    public async Task CreatesTag()
     {
         var request = new CreateTag.Request("hey");
         var response = await Fixture.SendAsync(request);
         Assert.False(response.IsError);
+        Assert.NotEqual(Guid.Empty, response.Value.Id);
         Assert.Equal("hey", response.Value.Name);
+    }
+
+    public sealed class RequestWithValidationErrors
+    {
+        [Fact]
+        public void ReturnsValidationError()
+        {
+            
+        }
     }
 }
