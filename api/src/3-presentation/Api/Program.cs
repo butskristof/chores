5fea85f5c876da817e81d2a79c2348c0cf383bb6
Kristof Buts <git@kristofbuts.be>
2023-12-23 15:58:00 +0100

    migrate database on startup (#13)

--- api/src/3-presentation/Api/Program.cs
+++ api/src/3-presentation/Api/Program.cs
@@ -4,6 +4,7 @@ using Chores.Application;
 using Chores.Application.Common.Constants;
 using Chores.Infrastructure;
 using Chores.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,27 +16,48 @@ builder
     .AddPersistence(builder.Configuration.GetConnectionString(ConfigurationConstants.AppDbContextConnectionStringKey))
     .AddApi();
 
-var app = builder.Build();
+try
+{
+    var app = builder.Build();
 
-app
-    // the default exception handler will catch unhandled exceptions and return 
-    // them as ProblemDetails with status code 500 Internal Server Error
-    .UseExceptionHandler()
-    // the status code pages will map additional failed requests (outside of
-    // those throwing exceptions) to responses with ProblemDetails body content
-    // this includes 404, method not allowed, ... (all status codes between 400 and 599)
-    // keep in mind that this middleware will only activate if the body is empty when
-    // it reaches it
-    .UseStatusCodePages();
+    app
+        // the default exception handler will catch unhandled exceptions and return 
+        // them as ProblemDetails with status code 500 Internal Server Error
+        .UseExceptionHandler()
+        // the status code pages will map additional failed requests (outside of
+        // those throwing exceptions) to responses with ProblemDetails body content
+        // this includes 404, method not allowed, ... (all status codes between 400 and 599)
+        // keep in mind that this middleware will only activate if the body is empty when
+        // it reaches it
+        .UseStatusCodePages();
 
-app.UseAuthorization();
-app.UseCors(ApplicationConstants.CorsPolicy);
+    app.UseAuthorization();
+    app.UseCors(ApplicationConstants.CorsPolicy);
 
 // add endpoint to retrieve OpenAPI definition
-app.MapSwagger();
+    app.MapSwagger();
 // add API endpoints
-app
-    .MapTagsEndpoints()
-    .MapChoresEndpoints();
+    app
+        .MapTagsEndpoints()
+        .MapChoresEndpoints();
 
-app.Run();
+    using (var scope = app.Services.CreateScope())
+    {
+        try
+        {
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            context.Database.Migrate();
+        }
+        catch (Exception)
+        {
+            // TODO log
+            throw;
+        }
+    }
+
+    app.Run();
+}
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    throw;
+}
