e3726c8c4917a02d17302374fb34702345c36161
Kristof Buts <git@kristofbuts.be>
2024-05-28 13:08:10 +0200

    add postgres migration

---
+++ api/src/2-infrastructure/Persistence/Migrations/20240528110743_Initial.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chores.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    Interval = table.Column<int>(type: "integer", nullable: false),
+                    Notes = table.Column<string>(type: "text", maxLength: -1, nullable: true),
+                    CreatedBy = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    LastModifiedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    Color = table.Column<string>(type: "character(7)", fixedLength: true, maxLength: 7, nullable: true),
+                    Icon = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    CreatedBy = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    LastModifiedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChoreIterations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ChoreId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Notes = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: true),
+                    CreatedBy = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: true),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: true),
+                    LastModifiedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChoreIterations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChoreIterations_Chores_ChoreId",
+                        column: x => x.ChoreId,
+                        principalTable: "Chores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChoreTags",
+                columns: table => new
+                {
+                    ChoreId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChoreTags", x => new { x.ChoreId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_ChoreTags_Chores_ChoreId",
+                        column: x => x.ChoreId,
+                        principalTable: "Chores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChoreTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChoreIterations_ChoreId",
+                table: "ChoreIterations",
+                column: "ChoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChoreTags_TagId",
+                table: "ChoreTags",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name_CreatedBy",
+                table: "Tags",
+                columns: new[] { "Name", "CreatedBy" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChoreIterations");
+
+            migrationBuilder.DropTable(
+                name: "ChoreTags");
+
+            migrationBuilder.DropTable(
+                name: "Chores");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
