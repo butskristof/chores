edc46c1061a3b4faa0ae500d3a0e7e952ac20591
Kristof Buts <git@kristofbuts.be>
2024-05-28 13:07:07 +0200

    switch to postgres provider

--- api/src/2-infrastructure/Persistence/DependencyInjection.cs
+++ api/src/2-infrastructure/Persistence/DependencyInjection.cs
@@ -3,8 +3,8 @@ using Chores.Application.Common.Persistence;
 using Chores.Persistence.Interceptors;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 
 namespace Chores.Persistence;
 
@@ -18,15 +18,15 @@ public static class DependencyInjection
             {
                 builder.AddInterceptors(serviceProvider.GetServices<ISaveChangesInterceptor>());
 
-                Action<SqlServerDbContextOptionsBuilder> sqlServerOptionsAction = optionsBuilder =>
+                Action<NpgsqlDbContextOptionsBuilder> npgsqlOptionsAction = optionsBuilder =>
                 {
                     optionsBuilder.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
                 };
 
                 if (connectionString is not null)
-                    builder.UseSqlServer(connectionString, sqlServerOptionsAction);
+                    builder.UseNpgsql(connectionString, npgsqlOptionsAction);
                 else if (connection is not null)
-                    builder.UseSqlServer(connection, sqlServerOptionsAction);
+                    builder.UseNpgsql(connection, npgsqlOptionsAction);
                 else
                     throw new ArgumentException("Missing connection details to set up persistence");
             });
